75d52dbf2e245d6599c15cdf71cdccb1408eab53
DaUmega <daumega@gmail.com>
2026-04-23 06:35:48 -0600

    fixed version number

--- internal/server/server.go
+++ internal/server/server.go
@@ -28,7 +28,7 @@ func New(agg *aggregator.Aggregator, mgr ExchangeManager) *Server {
 		agg:       agg,
 		mgr:       mgr,
 		startedAt: time.Now(),
-		version:   "2.0.0",
+		version:   "1.0.0",
 	}
 }
 
